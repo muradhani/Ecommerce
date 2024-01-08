7e869f2c715b9beb54fc05da9579d0a559142a64
murad <mouradhany574@gmail.com>
2024-01-08 13:06:15 +0200

    Data retrieved successfully and the problem was the suspend word in the api service file

--- app/src/main/java/com/example/ecommerce/activities/ShoppoingActivity.kt
+++ app/src/main/java/com/example/ecommerce/activities/ShoppoingActivity.kt
@@ -1,6 +1,7 @@
 package com.example.ecommerce.activities
 
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -23,6 +24,7 @@ class ShoppoingActivity : AppCompatActivity() {
         binding = ActivityShoppoingBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.bottomNav.setupWithNavController(findNavController(R.id.nav_host_fragment))
+        Log.i("main","hello")
     }
 
 }
