fd9fc1753f1acce87691d48e2b6a706426d0b4a9
murad <mouradhany574@gmail.com>
2024-02-12 23:37:29 +0200

    fixing the bottom navigation

--- app/src/main/java/com/example/ecommerce/activities/MainActivity.kt
+++ app/src/main/java/com/example/ecommerce/activities/MainActivity.kt
@@ -5,15 +5,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.FragmentContainerView
 import androidx.lifecycle.Observer
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavOptions
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.ecommerce.R
+import com.example.ecommerce.bindingAdapters.observeLogin
 import com.example.ecommerce.databinding.ActivityMainBinding
 import com.example.ecommerce.viewModels.LoginViewModel
 import com.example.ecommerce.viewModels.MainActivityViewModel
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 
@@ -32,10 +37,14 @@ class MainActivity : AppCompatActivity() {
         lifecycleScope.launch {
             viewModel.getLoginState()
         }
+        observeLogin()
     }
-
-//    override fun onBackPressed() {
-//        super.onBackPressed()
-//        findNavController(R.id.nav_host_fragment2).popBackStack()
-//    }
+    private fun observeLogin(){
+        viewModel.login.observe(this, Observer {
+            if (it){
+                val navController = binding.fragmentContainerLoginRegister.findNavController()
+                binding.bottomNav.setupWithNavController(navController)
+            }
+        })
+    }
 }
